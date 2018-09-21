866acbbab7959bcc7468e2abb12a4fe025432505
Steven Barnett <sbarnett@llnw.com>
2018-09-21 10:02:19 -0400

    Demo of issue

---
+++ app/initialize.js
@@ -0,0 +1,7 @@
+const Highcharts = require("highcharts");
+
+document.addEventListener('DOMContentLoaded', function() {
+  // do your setup here
+  console.log('Initialized app');
+  console.log(Highcharts);
+});
